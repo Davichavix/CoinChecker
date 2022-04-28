771a2b8d5980fe15af8d9d9bd38eed7cbc34daf3
David Chan <dchan902@gmail.com>
2022-04-27 22:03:09 -0600

    add Coin.jsx

---
+++ frontend/src/components/Coin.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './coin.css';
+
+const Coin = (props) => {
+  return (
+    <div className ='coin-container'>
+      <div className='coin-row'>
+        <div className='coin-type'>
+          <img src={props.image} alt='coin'></img>
+          <h1>{props.name}</h1>
+          <p className='symbol'>{props.symbol}</p>
+        </div>
+        <div className='coin-data'>
+          <div className='coin-price'>${props.price}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
