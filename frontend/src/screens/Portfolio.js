bfabd0abf73b9ee2075ed9da2fdcfbec5101ea95
lenilsamuel <lenilvarghese@gmail.com>
2022-04-27 20:03:47 -0600

    Add UserAvatar mui component

---
+++ frontend/src/screens/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserAvatar from '../components/UserAvatar'
+
+
+
+export const Portfolio = () => {
+  return (
+    <div>Portfolio
+      <UserAvatar />
+    </div>
+  )
+}
