7dc45229f9ca696715bcc86d765a734ffefd9974
jackiestchen <jackiestchen@gmail.com>
2022-04-28 22:35:04 -0600

    Feature: Add transaction model

---
+++ backend/models/transactionModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const transactionSchema = mongoose.Schema({
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Account",
+  },
+  holding: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Coin",
+  },
+  coin_amount: {
+    type: Number,
+    required: true,
+  },
+  cash_amount: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Transaction = mongoose.Schema("Transaction", transactionSchema);
+
+export default Transaction;
