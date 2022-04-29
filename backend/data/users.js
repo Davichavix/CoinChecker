d2d791a78a6504e14c5dcd208050764830fc3b9d
jackiestchen <jackiestchen@gmail.com>
2022-04-28 19:37:18 -0600

    Feature: Add User Model

---
+++ backend/data/users.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    name: "Lenil Samuel",
+    email: "123@example.com",
+    password: "$2a$10$CAwMIVksWq31gumxSGwHquBjSUywxwDtCwC4dq5niE2ddffXomc6y",
+  },
+  {
+    name: "Jacky Tchen",
+    email: "456@example.com",
+    password: "$2a$10$CAwMIVksWq31gumxSGwHquBjSUywxwDtCwC4dq5niE2ddffXomc6y",
+  },
+  {
+    name: "David Chan",
+    email: "789@example.com",
+    password: "$2a$10$CAwMIVksWq31gumxSGwHquBjSUywxwDtCwC4dq5niE2ddffXomc6y",
+  },
+];
