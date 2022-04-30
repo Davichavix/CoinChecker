7332a410688927b2566525669fe55d6bdb01e323
jackiestchen <jackiestchen@gmail.com>
2022-04-29 21:37:10 -0600

    Feature: Add user routes to server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import path from "path";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -16,6 +17,8 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
