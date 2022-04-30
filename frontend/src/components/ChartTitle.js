0316ca629ab61c93dbb876a4675b78da7de03922
lenilsamuel <lenilvarghese@gmail.com>
2022-04-30 11:54:19 -0600

    Change pie chart color to pull coin logo colors

--- frontend/src/components/ChartTitle.js
+++ frontend/src/components/ChartTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ChartTitle = ({title}) => {
   return (
-    <div style={{ marginTop: "20px", fontWeight: "bolder" }}>
+    <div style={{ marginTop: "20px", fontWeight: "bolder", fontSize: "1.5rem" }}>
       {title}
     </div>
   );
