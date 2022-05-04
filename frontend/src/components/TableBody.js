12bc26d7e13a6f1a4858404c318b05f68578358d
David Chan <dchan902@gmail.com>
2022-05-03 19:29:53 -0600

    modify table to have custom css for each column

--- frontend/src/components/TableBody.js
+++ frontend/src/components/TableBody.js
@@ -5,10 +5,16 @@ const TableBody = ({ tableData, columns }) => {
      return (
       <tr key={data.id}>
         <td key='image'><img src={data["image"]} alt = 'coin'></img></td>
-       {columns.map(({ accessor }) => {
+        <td key='name'>{data['name']}</td>
+        <td key='symbol'>{data['symbol']}</td>
+        <td key='price'>{data['current_price']}</td>
+        <td key='market-cap'>{(data['market_cap']/1000000000).toFixed(2)}B</td>
+        <td key='percent-move'>{data['price_change_percentage_24h']}</td>
+        <td key='total-volume'>{data['total_volume']}</td>
+       {/* {columns.map(({ accessor }) => {
         const tData = data[accessor] ? data[accessor] : "——";
         return <td key={accessor}>{tData}</td>;
-       })}
+       })} */}
       </tr>
      );
     })}
