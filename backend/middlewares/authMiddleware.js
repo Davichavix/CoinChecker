495548eee63201aba52c747b71e883a78cebfa47
jackiestchen <jackiestchen@gmail.com>
2022-05-04 23:14:01 -0600

    Feature: Add auth middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -3,4 +3,27 @@ import User from "../models/userModel";
 
 const protect = async (req, res, next) => {
   let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.starsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ");
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.find(decoded._id).select("-password");
+      next();
+    } catch (error) {
+      console.log(console.error());
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token!");
+  }
 };
+
+export { protect };
