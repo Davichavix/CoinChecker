1ec7e05f6e3683115d4ec2e4d8a4e6a73138186b
David Chan <dchan902@gmail.com>
2022-05-05 20:44:43 -0600

    modify search to include images

--- frontend/src/components/TableBody.js
+++ frontend/src/components/TableBody.js
@@ -19,7 +19,7 @@ const TableBody = ({ tableData, columns }) => {
               ${(data["market_cap"] / 1000000000).toFixed(2)}B
             </td>
             <td key="percent-move">
-              <div class="percent-move">
+              <div className="percent-move">
                 <div className={setPriceColor(data)}>
                   {data["price_change_percentage_24h"].toFixed(2)}%
                 </div>
