35472eabb7f26a14721a175ce61860f93f20d86d
lenilsamuel <lenilvarghese@gmail.com>
2022-05-05 19:32:10 -0600

    Add conditional styling to Snapshot

--- frontend/src/components/Snapshot.js
+++ frontend/src/components/Snapshot.js
@@ -4,10 +4,17 @@ import "../screens/styles/Portfolio.css";
 export const Snapshot = ({ label, dollars }) => {
   return (
     <div className="snapshot">
-      <div style={{ fontSize: "1.4rem", textTransform: "uppercase" }}>
-        {label}
+      <div>
+        <div style={{ fontSize: "1rem", fontWeight: "bold", textTransform: "uppercase" }}>
+          {label}
+        </div>
+        <div style={{ color: dollars.includes("-") ? "red" : "green", fontSize: "2rem" }}>
+          {dollars}
+        </div>
       </div>
-      <div>{dollars}</div>
+      <div
+        style={{ backgroundColor: dollars.includes("-") ? "red" : "green", height: "10px", marginTop: "10px" }}
+      ></div>
     </div>
   );
 };
