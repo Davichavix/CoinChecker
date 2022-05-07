5b52ed3b183a1ed3c6a8ce51b9a8e31840cf83f9
David Chan <dchan902@gmail.com>
2022-05-07 14:48:09 -0600

    import font-awesome library and use check

--- frontend/src/components/CheckIcon.js
+++ frontend/src/components/CheckIcon.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import { ReactComponent as CheckLogo } from "../assets/images/check-solid.svg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import "./CheckIcon.css"
 
 const CheckIcon = () => {
   return (
-      <div className="Check-icon">
-        <div className="Check-logo">
-          <CheckLogo />
+      <div>
+        <div className="Check-icon">
+        <FontAwesomeIcon
+          icon={faCheck}
+          size="lg"
+          style={{ color: "green" }}
+        />
         </div>
       </div>
   );
--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -11,7 +11,7 @@ const Table = () => {
   const [originalList, setOriginalList] = useState([]);
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h')
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=24h')
     .then(res => {
       setTableData(res.data)
       setOriginalList(res.data)
