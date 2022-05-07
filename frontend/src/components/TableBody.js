2aa15ff2db0536ff4a756898e0b6be01421dcb37
David Chan <dchan902@gmail.com>
2022-05-06 21:55:45 -0600

    modify table styling to be dark

--- frontend/src/components/TableBody.js
+++ frontend/src/components/TableBody.js
@@ -12,8 +12,8 @@ const TableBody = ({ tableData, columns }) => {
             <td key="image">
               <img className="coin-img" src={data["image"]} alt="coin"></img>
             </td>
-            <td key="name">{data["name"]}</td>
-            <td key="symbol">{data["symbol"]}</td>
+            <td className="coin-name" key="name">{data["name"]}</td>
+            <td className="coin-symbol" key="symbol">{data["symbol"]}</td>
             <td key="price">${data["current_price"]}</td>
             <td key="market-cap">
               ${(data["market_cap"] / 1000000000).toFixed(2)}B
