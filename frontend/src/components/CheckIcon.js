44c8cc4ba578ec5c31c89bb1d41a5be730802656
David Chan <dchan902@gmail.com>
2022-05-07 15:04:38 -0600

    add watchlist check toggle function

--- frontend/src/components/CheckIcon.js
+++ frontend/src/components/CheckIcon.js
@@ -5,14 +5,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import "./CheckIcon.css"
 
+
 const CheckIcon = () => {
+
+  const [click, setClick] = useState(false);
+
+  const changeColor = (click) => {
+    return click ? false : true;
+  }
+
+
   return (
       <div>
         <div className="Check-icon">
         <FontAwesomeIcon
           icon={faCheck}
-          size="lg"
-          style={{ color: "green" }}
+          size="xl"
+          style={{ color: click ? "green" : "white"}}
+          onClick={() => setClick(changeColor(click))}
         />
         </div>
       </div>
