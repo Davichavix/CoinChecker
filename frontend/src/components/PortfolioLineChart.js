a0af3e32cd25f022a2ff6afa5fe1bb7c91762d72
lenilsamuel <lenilvarghese@gmail.com>
2022-05-14 18:00:28 -0600

    Render 7-day data on line chart

--- frontend/src/components/PortfolioLineChart.js
+++ frontend/src/components/PortfolioLineChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -45,6 +45,7 @@ export const options = {
 
 const getDaysArray = function (start, end) {
   const arr = [];
+  const unixTime = []
   for (
     const dt = new Date(start);
     dt <= new Date(end);
@@ -52,18 +53,24 @@ const getDaysArray = function (start, end) {
   ) {
     const date = new Date(dt).toLocaleDateString('en-gb')
     arr.push(date.slice(0, 2) + '-' + date.slice(3, 5) + '-' + date.slice(6, 10));
+    unixTime.push(Math.round((new Date(dt)).getTime() / 1000))
+    
   }
-  return arr;
+  return {arr, unixTime};
 };
+
+
 
 const currentDate = new Date(Date.now());
 const sevenDaysPrior = new Date(
-  currentDate.getTime() - 7 * 24 * 60 * 60 * 1000
+  currentDate.getTime() - 14 * 24 * 60 * 60 * 1000
 );
 // console.log(sevenDaysPrior.toLocaleDateString(), "here");
 
-const labels = getDaysArray(sevenDaysPrior, currentDate.toLocaleDateString());
+const labels = getDaysArray(sevenDaysPrior, currentDate.toLocaleDateString()).arr;
+const unixTimesArr = getDaysArray(sevenDaysPrior, currentDate.toLocaleDateString()).unixTime;
 // console.log(labels, "labels");
+// console.log(test, "unix");
 const coinMap = {
   'btc' : 'bitcoin',
   'eth' : 'ethereum',
@@ -77,29 +84,62 @@ const coinMap = {
 
 
 export function PortfolioLineChart({holdingsMap}) {
+  const [xAxisData, setXAxisData] = useState([])
+  const [loading, setLoading] = useState(true)
+
   console.log(holdingsMap, "inside line chart");
   // console.log(labels, "inside");
   useEffect(() => {
+    const historicalPriceObj = {}
+    
     for (const label of labels) {
       const date = label;
       for (const key in holdingsMap) {
         // console.log(date, "date");
-        
+        // console.log(holdingsMap, "map inside");
         axios.get(`https://api.coingecko.com/api/v3/coins/${coinMap[key]}/history?date=${date}&localization=false`).then((res) => {
-          console.log(res.data.market_data.current_price.usd, "response");
+          // console.log(res.data.market_data.current_price.usd, "response", date, "date", key);
+          // if (!historicalPriceObj[date]) {
+          //   historicalPriceObj[date] =[res.data.market_data.current_price.usd * holdingsMap[key]]
+          // } else {
+          //   historicalPriceObj[date].push(res.data.market_data.current_price.usd * holdingsMap[key])
+          // }
+          if (!historicalPriceObj[date]) {
+            historicalPriceObj[date] =res.data.market_data.current_price.usd * holdingsMap[key]
+          } else {
+            historicalPriceObj[date] += res.data.market_data.current_price.usd * holdingsMap[key]
+          }
+        }).then(() => {
+          setXAxisData(Object.values(historicalPriceObj))
+      console.log(Object.values(historicalPriceObj, "got em"));
         })
       }
     }
+    console.log(historicalPriceObj, "this one");
+    // setLoading(false)
+    if (Object.keys(historicalPriceObj)) {
+      setXAxisData(Object.values(historicalPriceObj))
+      console.log(Object.values(historicalPriceObj, "got em"));
+    }
     
   }, [])
+
+  // if (Object.keys(historicalPriceObj)) {
+  //   // console.log('true');
+  //   setXAxisData(Object.values(historicalPriceObj))
+  //   console.log(Object.values(historicalPriceObj), "hereeee");
+  //   setLoading(false)
+  // }
+
   const data = {
     labels,
     datasets: [
       {
         label: "Value",
-        data: labels.map(
-          (label, i) => (i + 1) * Math.floor(Math.random() * 10000)
-        ),
+        data: xAxisData,
+        // data: labels.map(
+        //   (label, i) => (i + 1) * Math.floor(Math.random() * 10000)
+        // ),
         borderColor: "rgb(53, 162, 235)",
         backgroundColor: "rgba(53, 162, 235, 0.5)",
       },
@@ -107,6 +147,7 @@ export function PortfolioLineChart({holdingsMap}) {
   };
   return (
     <div style={{ marginTop: "50px" }}>
+      {/* {!loading && <Line options={options} data={data} />} */}
       <Line options={options} data={data} />
     </div>
   );
